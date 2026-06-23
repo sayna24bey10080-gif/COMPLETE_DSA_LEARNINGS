314b9bd307869755c9d4e7e3d13a20f08c76f558
SAYNA PATEL <sayna.24bey10080@vitbhopal.ac.in>
2026-06-23 14:03:34 +0530

    Time: 4 ms (51.82%), Space: 15 MB (18.76%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {}; // no solution (as per problem, won't happen)
+    }
+};
